d38b4be827835a8040a3cf083d4186b72912f46a
Nick <JBlythe425@gmail.com>
2019-09-03 18:44:33 -0700

    Update nicksMiniCalculator.cs

--- nicksMiniCalculator.cs
+++ nicksMiniCalculator.cs
@@ -7,36 +7,36 @@ namespace nicksMiniCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Nick's Mini-Calc\n");
-            bool UserContinue = true;
+            bool UserContinue = true; // Does user want to continue if yes return here
             do
             {
                 int x, y;
-                Console.Write("First Number: ");
+                Console.Write("First Number: "); // Our First Number
                 x = int.Parse(Console.ReadLine());
-                Console.Write("Second Number: ");
+                Console.Write("Second Number: "); // Our Second Number
                 y = int.Parse(Console.ReadLine());
-                Console.Write("Please Pick The Operation You Wish to Use?: (+,-,*,/,%) ");
+                Console.Write("Please Pick The Operation You Wish to Use?: (+,-,*,/,%) "); // Picking an Operation from one of the five options
                 string ops = Console.ReadLine();
 
-                calcFunction(x, y, ops); // Calls for the second function
+                calcFunction(x, y, ops); // Calls for the second function to calculate
 
                 Console.ReadLine();
-                Console.Write("Would You Like To Continue (y or n)?");
+                Console.Write("Would You Like To Continue (y or n)?"); // calling the bool to ask the user do you want to continue t is yes and n is false
                 string yesorno = Console.ReadLine();
 
                 if (yesorno == "y")
                 {
-                    UserContinue = true;
+                    UserContinue = true; // continue the program
                 }
                 else if (yesorno == "n")
                 {
-                    UserContinue = false;
-                    Console.WriteLine("Thank you & Come Back Soon! ^_^");
+                    UserContinue = false; // closes the program
+                    Console.WriteLine("Thank you & Come Back Soon! ^_^"); 
                     Console.ReadLine();
                 }
                 else
                 {
-                    Console.WriteLine("Error Invalid Input Please Try Again");
+                    Console.WriteLine("Error Invalid Input Please Try Again"); // Should an error occur in the process of continue or not this will pop-up
                 }
             } while (UserContinue == true);
 
